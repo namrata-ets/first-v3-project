58bf5188c612af568b70955eb1b2db8ceed358b0
namrata-ets <namrata.t.easternts@gmail.com>
2024-03-03 00:14:14 +0530

    -redundunt API call removed -added quasar UI components

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,75 @@
 <template>
-  <div>
+  <q-container>
     <q-banner :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'">
       <template v-slot:avatar>
-        <q-icon name="people" color="primary" />
+        <q-icon class="icon-position" name="people" color="primary" />
       </template>
-      Fetching User Data from a REST API
-      <template v-slot:action>
-        <q-btn color="primary" :loading="isLoading" label="Fetch Users" @click="fetchUsers"></q-btn>
+     <span class="title"> Fetching User Data from a REST API </span>
+     <template v-slot:action>
+        <q-btn class="button-position" color="primary" :loading="isLoading" label="Fetch Data" @click="fetchUsers"></q-btn>
       </template>
     </q-banner>
-    <h1>User Data</h1>
-    
+
     <ul v-if="users">
-      <span v-for="(user,index) in users" :key="user.id">
-        <User :user="user" :userId="user.id" :userIndex="index"/>
+      <span v-for="(user, index) in users" :key="user.id">
+        <User :user="user" />
       </span>
     </ul>
-  </div>
-
+  </q-container>
 </template>
 
 <script setup>
-
 import { ref } from 'vue';
 import axios from 'axios';
 import User from './components/User.vue';
 
 const users = ref([]);
 const isLoading=ref(false);
-
-const fetchUsers = async () => {
+const error=ref('');
+const fetchUsers = async () =>{
   try {
-    isLoading.value = true;
     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+     //check if the API call is successful status code will be from 200 to 299, else any other status code that indicates API request unsuccessful
+      if (!response.status.toString().startsWith('2')) {
+          throw new Error(`API error: ${response.status} - ${response.statusText}`);
+      }
     users.value = response.data;
-    isLoading.value = false;
-  } catch (error) {
-    isLoading.value = false;
-    console.error('Error fetching posts:', error);
   }
-}
+  catch (error) {
+      isLoading.value = false;
+      alert=true;
+      console.error('Error fetching users:', error);
+        if (error.isAxiosError && error.response) {
+              //Check for Network errors or other status code errors
+              console.error(`Network error (${error.response.status}): ${error.response.data}`);
+            } else {
+              // Log any other errors
+              console.error('Unexpected error:', error);
+            }
+  }
+  finally {
+      isLoading.value = false;
+    }
+};
+
+
 </script>
+<style>
+.title{
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-weight: bold;
+  color: black;
+  font-size: 50px;
+  align-items: center;
+  padding-left: 100px;
+}
+.icon-position{
+      padding-top: 50px;
+}
+.button-position{
+    margin-right: 150px;
+    margin-top: 50px;
+    margin-bottom: 40px;
+}
+</style>
+
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,53 +1,43 @@
-<template>
+  <template>
     <div class="q-pa-md" style="max-width: 350px">
-    <q-list dense bordered padding class="rounded-borders">
-      <q-item clickable v-ripple>
-        <q-item-section>
-          {{ user.id }}
-        </q-item-section>
-      </q-item>
+      <q-list dense bordered padding class="item-section rounded-borders">
+        <q-item clickable v-ripple class="">
+          <q-item-section>
+            {{ user.id }}
+          </q-item-section>
+        </q-item>
 
-      <q-item clickable v-ripple>
-        <q-item-section>
-          {{ user.name }}
-        </q-item-section>
-      </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section>
+           Name: {{ user.name }}
+          </q-item-section>
+        </q-item>
 
-      <q-item clickable v-ripple>
-        <q-item-section>
-          {{ user.email }}
-        </q-item-section>
-      </q-item>
-    </q-list>
-  <div v-if="error" class="error">Error: {{ error }}</div>
-  </div>
+        <q-item clickable v-ripple>
+          <q-item-section>
+            Email: {{ user.email }}
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import axios from 'axios';
 
-  const user = ref([{}]);
-  const isLoading=ref(false);
+  <script setup>
+  import { defineProps } from 'vue';
+
   const props = defineProps({
-  userId: Number,
-  userIndex: Number
-});
-  
-  const fetchUser = async () => {
-    try {
-      isLoading.value = true;
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${props.userId}`);
-      user.value = response.data;
-      isLoading.value = false;
-    } catch (error) {
-      isLoading.value = false;
-      console.error('Error fetching post:', error);
-    }
-  }
-  
- 
-  fetchUser(props.userId); // Fetch user data when component is mounted, using the provided userId
-   
+    user: Object
+  });
   </script>
-  
+
+  <style>
+  .item {
+    color: red;
+  }
+
+  .item-section {
+    border-color: blue;
+    border-style: double;
+  }
+  </style>
+
